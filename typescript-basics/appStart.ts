0a9c48ee2384cda6b1e8dcf0d19589990d0bd81a
Alexander-Gabor <gaboralexanderalexgmail.com>
2024-04-25 13:18:00 +0200

    Test Typescript types

---
+++ typescript-basics/appStart.ts
@@ -0,0 +1,17 @@
+let variable2 = "Hey";
+variable2 = "Nope";
+
+let myAge: number;
+// myAge = 25;
+// myAge = "Yes"
+
+let testStringOrNumber2: string | number;
+
+let namesArray = ["john", "jane", "tom"];
+
+namesArray.push("Mike");
+
+// let numbersArray = [11, 22, 35];
+
+// numbersArray.push("Alex");
+
